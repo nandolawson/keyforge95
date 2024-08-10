9a107eab01b3b5917e803f630c09309067f87e09
Nando <37831410+nandolawson@users.noreply.github.com>
2024-08-10 20:15:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# keyforge95
+This library is a cross-plattform generator and validator for Windows 95 keys written in Rust. As it turned out, however, it supposedly works for all Microsoft products that require a 10-digit product key in the following format: _`XXX-XXXXXXX`_
+
+It is only sporadically developed as it is solely a learning project for me and does not have particularly significant practical use.
+⚠️ **Note**: keyforge95 and I are in no way associated with Microsoft or their products. Only [publicly available information](https://en.wikipedia.org/wiki/Product_key#Examples) from the internet has been used to create this project. It neither bypasses effective copy measures nor constitutes a "crack." The purpose of this repository, from my perspective, is solely to gain experience in programming and enhance my skills and knowledge.
+
+## Compatible Software
+
+- Access ADI 95
+- Hell Bender
+- Office 7.0b
+- Office Professional 95
+- Plus! 95
+- Plus! 98
+- Return to Arcade
+- Windows 95
+- Windows CE Toolkit for Visual Basic 5
+- Windows CE Toolkit for Visual C++ 5
+- Windows NT 4.0 Server
+- Windows NT 4.0 Workstation
+- Visual Basic Standard 4.0
+- Visual SourceSafe 4.0
+
+## Usage
+
+This library only has two public functions: _`generate_product_key()`_ and _`validate_product_key()`_
+
+### Generate
+
+After adding keyforge95 to your project, just use _`generate_product_key()`_ to generate a valid product key as a String.
+
+```rs
+use keyforge95::generate_product_key;
+let product_key: String = generate_product_key();
+println("Generated product key: {}", product_key);
+```
+
+### Validate
+
+To check the validity of a key, add keyforge95 to your project and use _`validate_product_key("key")`_. This function returns a bool. It is important that the right formatting (_`XXX-XXXXXXX`_) is used for the product key. Otherwise the validation will fail.
+
+```rs
+use keyforge95::validate_product_key;
+let product_key: &str = 000-0000000
+match validate_product_key(product_key) {
+    true => println!("Valid key: {}", product_key),
+    false => println!("Invalid key: {}", product_key)
+}
+```
+
+## Contributing
+
+Anyone who wants to contribute is more than welcome to do so. I would be delighted to learn from the contributions of other users. If you find a bug or have a feature in mind that you think would be useful, please feel free to create an issue or a pull request here on GitHub.
+If you decide to fork this project, please make sure to adhere to the [license](https://github.com/nandolawson/keyforge95/blob/master/LICENSE). Your involvement and feedback are highly appreciated!
---
+++ src/lib.rs
@@ -0,0 +1,174 @@
+/*
+keyforge95
+Copyright 2024 Nando Lawson
+
+Licensed under the GPL, Version 3 <https://opensource.org/license/gpl-3-0>.
+This file may not be copied, modified, or distributed except according to those terms.
+*/
+
+#![doc(
+    html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk.png",
+    html_favicon_url = "https://www.rust-lang.org/favicon.ico",
+)]
+#![doc = include_str!("../README.md")] // Adding the README to the documentation
+
+// Dependencies
+use std::ops::RangeInclusive;
+use rand::Rng;
+
+//Public Functions
+
+/// Tests if a product key is valid
+/// 
+/// # Examples
+/// ```
+/// use keyforge95::validate_product_key;
+/// let test_cases: [&str; 5] = [ // This keys should be valid
+///     "111-1111111",
+///     "000-0000000",
+///     "332-3333333",
+///     "334-7777777",
+///     "901-2345678"
+/// ];
+/// for test_case in test_cases {
+///      assert_eq!(validate_product_key(test_case), true);
+/// }
+/// ```
+/// 
+/// ```
+/// use keyforge95::validate_product_key;
+/// let test_cases: [&str; 5] = [ // This keys should be invalid
+///     "111-1111112",
+///     "ABC-DEF-GHI",
+///     "333-3333333",
+///     "111-7777769",
+///     "123-4567-89-00-AB4"
+/// ];
+/// for test_case in test_cases {
+///     assert_eq!(validate_product_key(test_case), false);
+/// }
+/// ```
+pub fn validate_product_key(product_key: &str) -> bool {
+    match validate_format(product_key) { // Check if the product key format is valid
+        true => {
+            matches!((
+                validate_block(&product_key[0..=2]), // Check if first block is valid
+                validate_block(&product_key[4..=10]) // Check if second block is valid
+            ), (true, true))
+        }
+        false => false
+    }
+}
+
+/// Generates a valid product key
+///
+/// # Example
+///
+/// ```
+/// use keyforge95::generate_product_key;
+/// for _ in 0..10 {
+///     let product_key: String = generate_product_key();
+///     assert_eq!(product_key.len(), 11);
+///     assert_eq!(product_key.chars().nth(3).unwrap(), '-');
+/// }
+/// ```
+pub fn generate_product_key() -> String {
+    // Use generate_block() for product key generation and print it with the right format
+    format!("{}-{}", generate_block("a"), generate_block("b"))
+}
+
+// Functions
+
+fn validate_format(product_key: &str) -> bool {
+    if product_key.len() == 11 { // The length of the product key must be 11 digits
+        matches!((
+            product_key[0..=2].chars().all(|c| c.is_numeric()), // Block must not contain anything else than numbers
+            product_key[4..=10].chars().all(|c| c.is_numeric()), // Same rule for block b
+            product_key.chars().nth(3) == Some('-'), // The fourth character must be a tie rope
+        ), (true, true, true))
+    } else {false}
+}
+
+fn validate_block(block: &str) -> bool {
+    // First determine the block of the product key per length of the block
+    if block.len() == 3 { // Block a
+        !((3..=9).contains(&(block.parse::<i32>().unwrap() / 111)) // This block must not contain a lucky number between 333 and 999
+            && block.parse::<i32>().unwrap() % 111 == 0)
+    } else if block.len() == 7 { // Block b
+        matches!((
+            block.contains('9'), // The number 9 is not allowed for this block
+            block.chars().filter_map(|c| c.to_digit(10)).sum::<u32>() % 7 == 0 // The sum of this block must be divisible by 7 with no remainder
+        ), (false, true))
+    } else { // Only used if the block size is neither 3 nor 7
+        false
+    }
+}
+
+fn generate_block(choice: &str) -> String {
+    // Determine which block of the product key will be generated
+    let range: RangeInclusive<u32> = if choice == "a" {
+        000..=998 // The number range for block a
+    } else {
+        0000000..=8888888 // The number range for block b
+    };
+    let length: usize = if choice == "a" {
+        3 // The length of block a
+    } else { // Lieber einen Error Code als dass alles bis auf a direkt b ist
+        7 // The length of block b
+    };
+    // Generate a block and validate it
+    loop { // Loop this operation if it fails
+        let block: String = format!("{:0length$}", rand::thread_rng().gen_range(range.clone())); // Generate a block of the product key
+           if validate_block(&block) {
+            return block // Exit the loop if the block validates successfully
+        }
+    }
+}
+
+// Tests
+
+#[test]
+fn test_validate_format() {
+    let product_key: &str = "000-0000000"; // This key should be formatted correctly
+    assert_eq!(validate_product_key(product_key), true);
+    let test_cases: [&str; 5] = [ // This keys should be formatted incorrectly
+        "000-00000000",
+        "0000-0000000",
+        "0-0",
+        "A00-B000000",
+        "A-A-A-A-A"
+    ];
+    for test_case in test_cases {
+        assert_eq!(validate_format(test_case), false);
+    }
+}
+#[test]
+fn test_validate_block() {
+    let test_cases: [&str; 5] = [ // This blocks should be valid
+        "111",
+        "334",
+        "998",
+        "1111111",
+        "8888888"
+    ];
+    for test_case in test_cases {
+        assert_eq!(validate_block(test_case), true);
+    }
+    let test_cases: [&str; 5] = [ // This blocks should be invalid
+        "333",
+        "999",
+        "0",
+        "9999999",
+        "000000"
+    ];
+    for test_case in test_cases {
+        assert_eq!(validate_block(test_case), false);
+    }
+}
+#[test]
+fn test_generate_block() {
+    for _ in 0..10 { // Generates both blocks
+        assert_eq!(generate_block("a").len(), 3); // First block
+        assert_eq!(generate_block("b").len(), 7); // Second block
+    }
+}
