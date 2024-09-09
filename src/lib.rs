56403e609c80130f5da5981f947c403a849979b3
Nando <mail@nandolawson.online>
2024-09-09 16:08:23 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ This file may not be copied, modified, or distributed except according to those 
 #![doc = include_str!("../README.md")] // Adding the README to the documentation
 
 // Dependencies
+
 use core::ops::RangeInclusive;
 use std::process::exit;
 use rand::Rng;
